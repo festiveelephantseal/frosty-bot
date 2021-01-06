5f46926617554da6b0f3cfc37f7c7d59c57c9fee
nerdthatnoonelikes <kenrag24@gmail.com>
2021-01-06 14:03:04 -0600

    setup prisma

--- src/Bot.ts
+++ src/Bot.ts
@@ -2,4 +2,10 @@ import { token, owners } from "../config";
 import BotClient from "./lib/Client";
 
 const client: BotClient = new BotClient({token, owners});
-client.start();
+(async () => {
+    await client.prisma
+        .$connect()
+        .then(() => client.logger.info("Connected to postgresql"))
+
+    client.start();
+})();
--- src/lib/typings/Akairo.ts
+++ src/lib/typings/Akairo.ts
@@ -1,11 +1,13 @@
 import Logger from "@ayanaware/logger";
 import { BotOptions } from "../interfaces/BotOptions";
+import { PrismaClient } from "@prisma/client"
 
 declare module "discord-akairo" {
     interface AkairoClient {
         commandHandler: CommandHandler,
         listenerHandler: ListenerHandler,
         logger: Logger,
-        config: BotOptions
+        config: BotOptions,
+        prisma: PrismaClient
     }
 }
