0bee108f95d3872fdd13e045af626b68d8c6ed0c
nerdthatnoonelikes <kenrag24@gmail.com>
2021-01-06 14:04:33 -0600

    Revert "setup prisma"
    This reverts commit 5f46926617554da6b0f3cfc37f7c7d59c57c9fee.

--- src/Bot.ts
+++ src/Bot.ts
@@ -2,10 +2,4 @@ import { token, owners } from "../config";
 import BotClient from "./lib/Client";
 
 const client: BotClient = new BotClient({token, owners});
-(async () => {
-    await client.prisma
-        .$connect()
-        .then(() => client.logger.info("Connected to postgresql"))
-
-    client.start();
-})();
+client.start();
--- src/lib/typings/Akairo.ts
+++ src/lib/typings/Akairo.ts
@@ -1,13 +1,11 @@
 import Logger from "@ayanaware/logger";
 import { BotOptions } from "../interfaces/BotOptions";
-import { PrismaClient } from "@prisma/client"
 
 declare module "discord-akairo" {
     interface AkairoClient {
         commandHandler: CommandHandler,
         listenerHandler: ListenerHandler,
         logger: Logger,
-        config: BotOptions,
-        prisma: PrismaClient
+        config: BotOptions
     }
 }
