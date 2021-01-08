22ec12bd7ffd2744b219ab4a2afd36599d5b9c75
nerdthatnoonelikes <kenrag24@gmail.com>
2021-01-08 20:08:09 +0000

    feat(Cooldowns): Added a cooldown listener

---
+++ src/core/listeners/Cooldown.ts
@@ -0,0 +1,16 @@
+import { Message } from "discord.js"
+import { Listener } from "discord-akairo";
+
+export default class CommandCooldown extends Listener {
+    constructor() {
+        super("cooldown", {
+            emitter: "commandHandler",
+            event: "cooldown"
+        });
+    }
+
+    public exec(message: Message, remaining: number) {
+        const final = remaining/1000
+        message.util.send(`You can use that command in \`${final} seconds\``);
+    }
+}
