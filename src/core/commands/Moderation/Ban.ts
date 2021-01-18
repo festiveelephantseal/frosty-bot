d720f87eef5d9ff29f78153730d4044db7516134
nerdthatnoonelikes <kenrag24@gmail.com>
2021-01-18 12:05:03 -0600

    ban

---
+++ src/core/commands/Moderation/Ban.ts
@@ -0,0 +1,60 @@
+import { Command } from "discord-akairo";
+import { Message, MessageEmbed, GuildMember } from "discord.js";
+
+export default class Ban extends Command {
+    public constructor() {
+        super("ban", {
+            aliases: ["ban"],
+            category: "Moderation",
+            description: {
+                content: "BAAAAAAAN",
+            },
+            args: [
+                {
+                    id: "member",
+                    type: "member",
+                    prompt: {
+                        start: (msg: Message) =>
+                            `${msg.author} Please provide a member to ban`,
+                        retry: (msg: Message) =>
+                            `${msg.author} thats not a valid member to ban`,
+                    },
+                },
+                {
+                    id: "reason",
+                    match: "flag",
+                    flag: "--reason",
+                },
+            ],
+            clientPermissions: ["BAN_MEMBERS"],
+            userPermissions: ["BAN_MEMBERS"],
+            channel: "guild",
+        });
+    }
+
+    public async exec(
+        message: Message,
+        { member, reason }: { member: GuildMember; reason: string }
+    ) {
+        const clientUser = await message.guild.members.fetch(
+            this.client.user.id
+        );
+        if (member.bannable)
+            return message.util.send(":x: This member can't be banned");
+
+        if (!clientUser.permissions.has("BAN_MEMBERS"))
+            return message.util.send(":x: I don't have permission to do this!");
+
+        try {
+            member.ban();
+            message.util.send(
+                new MessageEmbed()
+                    .setAuthor(
+                        `${member.user.username} was banned!`,
+                        member.user.displayAvatarURL({ dynamic: true })
+                    )
+                    .setDescription(`Reason: ${reason}`)
+            );
+        } catch (e) {}
+    }
+}
