bd8d07840184c173c0ccad5576488fe2421aced7
nerdthatnoonelikes <kenrag24@gmail.com>
2021-01-17 18:03:36 -0600

    fix balance command

--- src/core/commands/Economy/Balance.ts
+++ src/core/commands/Economy/Balance.ts
@@ -8,25 +8,25 @@ export default class BalanceCommand extends Command {
             aliases: ["balance", "bal", "coins"],
             category: "Econnomy",
             description: {
-                content: "See how much coins you or another user have"
+                content: "See how much coins you or another user have",
             },
             args: [
                 {
                     id: "member",
                     type: "member",
-                    default: (msg: Message) => msg.author
-                }
-            ]
-        })
+                    default: (msg: Message) => msg.author,
+                },
+            ],
+        });
     }
-    public async exec(message: Message, { member } : { member: GuildMember}) {
+    public async exec(message: Message, { member }: { member: GuildMember }) {
         if (member.user.bot) {
             return message.channel.send("Bots can't have money >:(");
+        } else {
+            const coins = await getCoins(member.id);
+            const { user } = message.guild.members.cache.get(member.id);
+
+            message.reply(`${user.username} has ${coins} coins`);
         }
-
-        const coins = await getCoins(member.id);
-        const { user } = message.guild.members.cache.get(member.id);
-
-        message.reply(`${user.username} has ${coins} coins`)
     }
 }
