77c0da053d410c2234d545d8b585cd9c1e95389b
nerdthatnoonelikes <kenrag24@gmail.com>
2021-02-01 15:12:16 +0000

    message update and delete listeners

---
+++ src/core/listeners/MessageDelete.ts
@@ -0,0 +1,34 @@
+import { Listener } from "discord-akairo";
+import { Message, MessageEmbed, TextChannel } from "discord.js";
+import LogChannelSchema from "../../lib/schemas/LogChannelSchema";
+
+export default class MessageDeleteListener extends Listener {
+    public constructor() {
+        super("messageDelete", {
+            emitter: "client",
+            event: "messageDelete"
+        })
+    }
+
+    public async exec(message: Message) {
+        const result = await LogChannelSchema.findOne({
+            guildID: message.guild.id
+        })
+
+        if (!result) return;
+
+        if (message.author.bot) return;
+
+        if (result) {
+            const embed: MessageEmbed = new MessageEmbed()
+            .setTitle("Message Deleted")
+            .setColor("BLUE")
+            .setDescription(`${message.author.tag} deleted a message in <#${message.channel.id}>`)
+            .addField("Content", message.content, true)
+
+            // @ts-ignore
+            const channel = this.client.channels.cache.get(result.logChannel) as TextChannel
+            channel.send(embed);
+        }
+    }
+}
