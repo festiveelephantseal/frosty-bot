72f03e064242695d842866dfd45e01562345c508
nerdthatnoonelikes <kenrag24@gmail.com>
2021-02-05 16:57:45 +0000

    delete inventory model and interface, scrapped the commands

--- src/lib/models/InventoryModel.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-import { IInventorySchema } from "../interfaces/IInventorySchema";
-
-const InventorySchema = new Schema<IInventorySchema>({
-  userID: { required: true, type: String },
-  items: { required: true, type: [{ item: String, amount: Number }] },
-});
-
-export default model("Inventory", InventorySchema);
